86a162b4bf9a571f806aea12a2bce322fbb1c525
Abhijitgavale01 <128979169+Abhijitgavale01@users.noreply.github.com>
2023-03-26 18:43:40 +0530

    Add files via upload

---
+++ arithmetic  operators .c
@@ -0,0 +1,33 @@
+//arithmetic  operators  example
+#include<stdio.h>
+  void main()
+
+ {  
+int a,b,c,d;
+a=50;
+b=40;
+printf("sum=%d\n",(a+b));
+printf("sub=%d\n",(a-b));
+printf("mul=%d\n",(a*b));
+printf("div=%d\n",(a/b));
+printf("mod=%d\n",(a%b));
+
+
+   
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+    
